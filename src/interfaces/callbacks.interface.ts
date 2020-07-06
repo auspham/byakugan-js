c997022074f08ee2413831450e8cbc9488e720d6
rockmanvnx6 <rockmanvnx6@gmail.com>
2020-07-06 15:39:20 +1000

    callbacks commenting

--- src/interfaces/callbacks.interface.ts
+++ src/interfaces/callbacks.interface.ts
@@ -1,5 +1,11 @@
 import { Node } from '../components/node';
 
+/**
+ * List of available callbacks.
+ *
+ * @export
+ * @interface Callbacks
+ */
 export interface Callbacks {
     nodeConstructions?: (node: Object) => any;
 }
