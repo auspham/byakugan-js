617604ab454961041242f4a5eebbbc9433a8b5f4
Austin <rockmanvnx6@gmail.com>
2020-07-07 17:08:38 +1000

    update sprite

--- examples/scripts/tiles.js
+++ examples/scripts/tiles.js
@@ -1,25 +1,27 @@
 let img;
 
 function setup() {
-    console.log("aaa");
-    createCanvas(768, 768);
+    createCanvas(img.width, img.height);
+    console.log(img.width, img.height)
     image(img, 0, 0, img.width, img.height);
-    let bsize = 48;
-    let row = img.width / bsize;
-    let col = img.height / bsize;
+    let bsize = 16;
+    let row = img.height / bsize;
+    let col = img.width / bsize;
     let count = 0;
     for(let i = 0; i < row; i++) {
         for(let j = 0; j < col; j++) {
             stroke(0);
             noFill();
-            rect(j * bsize, i * bsize, bsize, bsize);
+            // rect(j * bsize, i * bsize, bsize, bsize);
             fill(0,255,0);
-            text(count, j * bsize, i * bsize, CENTER, CENTER);
+            textAlign(CENTER, CENTER);
+            textSize(6);
+            text(count, j * bsize + bsize / 2, i * bsize + bsize / 2);
             count++;
         }
     }
 }
 
 function preload() {
-    img = loadImage("./assets/48x48.png");
+    img = loadImage("./assets/tiles.png");
 }
